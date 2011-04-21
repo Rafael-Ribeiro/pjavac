008ae4844ffb973f551d0ad186cf26c2e9d59e53
jprafael <jprafael@student.dei.uc.pt>
2011-04-21 21:23:19 +0100

    finished?

--- src/free.c
+++ src/free.c
@@ -4,422 +4,445 @@
 #include "inc/structures.h"
 #include "inc/free.h"
 
-void free_application(is_application* application)
+void free_application(is_application* node)
 {
+	free_class_def(node);
 }
 
 void free_id(is_id* node)
 {
-	free(node->name);
-	free(node);
+	if (node)
+	{
+		free(node->name);
+		free(node);
+	}
 }
 
 void free_constant(is_constant* node)
 {
-	/* TODO */
-}
+	if (node)
+	{
+		switch (node->type)
+		{
+			case t_constant_char:
+			case t_constant_string:
+				free(node->value.string_val);
+				break;
 
-void free_constant_bool(is_constant* node)
-{
-	free(node);
-}
-
-void free_constant_long(is_constant* node)
-{
-	free(node);
-}
-
-void free_constant_double(is_constant* node)
-{
-	free(node);
-}
-
-void free_constant_char(is_constant* node)
-{
-	free(node->value.string_val);
-	free(node);
-}
-
-void free_constant_string(is_constant* node)
-{
-	free(node->value.string_val);
-	free(node);
+			default:
+				break;
+		}
+		free(node);
+	}
 }
 
 void free_array_decl (is_array_decl* node)
 {
-	free_type_object(node->type);
-	free(node);
+	if (node)
+	{
+		free_type_object(node->type);
+		free(node);
+	}
 }
 
 void free_assign_op (is_assign_op* node)
 {
-	free_var(node->var);
-	free_expr(node->expr);
+	if (node)
+	{
+		free_var(node->var);
+		free_expr(node->expr);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_binary_op(is_binary_op* node)
 {
-	/* TODO */
-}
+	if (node)
+	{
+		switch (node->type)
+		{
+			case t_binary_op_assign:
+				free_assign_op(node->data.assign);
+				break;
 
-void free_binary_op_operation (is_binary_op* node)
-{
-	free_expr(node->data.operands.left);
-	free_expr(node->data.operands.right);
+			default:
+				free_expr(node->data.operands.left);
+				free_expr(node->data.operands.right);
+				break;
+		}
 
-	free(node);
-}
-
-void free_binary_op_assign (is_binary_op* node)
-{
-	free_assign_op(node->data.assign);
-	free(node);
+		free(node);
+	}
 }
 
 void free_break (is_break* node)
 {
-	free_id(node->label);
-	free(node);
+	if (node)
+	{
+		free_id(node->label);
+		free(node);
+	}
 }
 
 void free_class_stmt_list (is_class_stmt_list* node)
 {
-	free_class_stmt_list(node->next);
-	free_class_stmt(node->node);
+	if (node)
+	{
+		free_class_stmt_list(node->next);
+		free_class_stmt(node->node);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_class_def (is_class_def* node)
 {
-	free_id(node->id);
-	free_class_stmt_list(node->body);
+	if (node)
+	{
+		free_id(node->id);
+		free_class_stmt_list(node->body);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_class_stmt (is_class_stmt* node)
 {
-	free_class_stmt_privacy(node->privacy);
-	free_class_stmt_scope(node->scope);
-	free_member_stmt(node->stmt);
+	if (node)
+	{
+		free_class_stmt_privacy(node->privacy);
+		free_class_stmt_scope(node->scope);
+		free_member_stmt(node->stmt);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_class_stmt_privacy(is_class_stmt_privacy* node)
 {
-	free(node);
+	if (node)
+		free(node);
 }
 
 void free_class_stmt_scope (is_class_stmt_scope* node)
 {
-	free(node);
+	if (node)
+		free(node);
 }
 
 void free_continue (is_continue* node)
 {
-	free_id(node->label);
+	if (node)
+	{
+		free_id(node->label);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_dims (is_dims* node)
 {
-	free_dims_sized_list(node->sized);
+	if (node)
+	{
+		free_dims_sized_list(node->sized);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_dims_sized (is_dims_sized* node)
 {
-	/* TODO */
+	free_expr(node);
 }
 
 void free_dims_sized_list (is_dims_sized_list* node)
 {
-	free_dims_sized(node->node);
-
-	if (node->next)
+	if (node)
+	{
+		free_dims_sized(node->node);
 		free_dims_sized_list(node->next);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_do_while (is_do_while* node)
 {
-	free_stmt(node->body);
-	free_expr(node->cond);
+	if (node)
+	{
+		free_stmt(node->body);
+		free_expr(node->cond);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_expr(is_expr* node)
 {
-	/* TODO */
-}
+	if (node)
+	{
+		switch (node->type)
+		{
+			case t_expr_var:
+				free_var(node->data.var);
+				break;
 
-void free_expr_var(is_expr* node)
-{
-	free_var(node->data.var);
+			case t_expr_new_op:
+				free_new_op(node->data.new_op);
+				break;
 
-	free(node);
-}
+			case t_expr_type_cast:
+				free_type_decl(node->data.type_cast.type);
+				free_expr(node->data.type_cast.expr);
+				break;
 
-void free_expr_new_op(is_expr* node)
-{
-	free_new_op(node->data.new_op);
+			case t_expr_constant:
+				free_constant(node->data.constant);
+				break;
 
-	free(node);
-}
+			case t_expr_func_call:
+				free_func_call(node->data.func_call);
+				break;
 
-void free_expr_constant(is_expr* node)
-{
-	free_constant(node->data.constant);
-
-	free(node);
-}
-
-void free_expr_func_call(is_expr* node)
-{
-	free_func_call(node->data.func_call);
-
-	free(node);
-}
-
-void free_expr_expr_op(is_expr* node)
-{
-	free_expr_op(node->data.operation);
-
-	free(node);
+			case t_expr_operation:
+				free_expr_op(node->data.operation);
+				break;
+		}
+	}
 }
 
 void free_expr_list (is_expr_list* node)
 {
-	free_expr(node->node);
-
-	if(node->next)
+	if (node)
+	{
+		free_expr(node->node);
 		free_expr_list(node->next);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_expr_op (is_expr_op* node)
 {
-	/* TODO */
-}
+	if (node)
+	{
+		switch (node->type)
+		{
+			case t_expr_op_unary:
+				free_unary_op(node->data.unary);
+				break;
 
-void free_expr_op_unary (is_expr_op* node)
-{
-	free_unary_op(node->data.unary);
+			case t_expr_op_binary:
+				free_binary_op(node->data.binary);
+				break;
 
-	free(node);
-}
+			case t_expr_op_ternary:
+				free_ternary_op(node->data.ternary);
+				break;
+		}
 
-void free_expr_op_binary(is_expr_op* node)
-{
-	free_binary_op(node->data.binary);
-
-	free(node);
-}
-
-void free_expr_op_ternary(is_expr_op* node)
-{
-	free_ternary_op(node->data.ternary);
-
-	free(node);
+		free(node);
+	}
 }
 
 void free_for(is_for* node)
 {
-	free_for_init(node->init);
-	free_for_cond(node->cond);
-	free_for_inc(node->inc);
-	free_stmt(node->body);
+	if (node)
+	{
+		free_for_init(node->init);
+		free_for_cond(node->cond);
+		free_for_inc(node->inc);
+		free_stmt(node->body);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_for_cond(is_for_cond* node)
 {
-	/* TODO */
+	free_expr(node);
 }
 
 void free_for_expr(is_for_expr *node)
 {
-	/* TODO */
-}
+	if (node)
+	{
+		switch (node->type)
+		{
+			case t_for_expr_incr:
+				free_incr_op(node->data.incr);
+				break;
 
-void free_for_expr_incr(is_for_expr* node)
-{
-	free_incr_op(node->data.incr);
+			case t_for_expr_assign:
+				free_assign_op(node->data.assign);
+				break;
 
-	free(node);
-}
+			case t_for_expr_func_call:
+				free_func_call(node->data.func_call);
+				break;
+		}
 
-void free_for_expr_assign(is_for_expr* node)
-{
-	free_assign_op(node->data.assign);
-
-	free(node);
-}
-
-void free_for_expr_func_call(is_for_expr* node)
-{
-	free_func_call(node->data.func_call);
-
-	free(node);
+		free(node);
+	}
 }
 
 void free_for_expr_list(is_for_expr_list* node)
 {
-	free_for_expr(node->node);
-
-	if (node->next)
+	if (node)
+	{
+		free_for_expr(node->node);
 		free_for_expr_list(node->next);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_for_init(is_for_init* node)
 {
-	/* TODO */
-}
+	if (node)
+	{
+		switch (node->type)
+		{
+			case t_for_init_var_defs:
+				free_var_defs(node->data.var_defs);
+				break;
 
-void free_for_init_var_defs(is_for_init* node)
-{
-	free_var_defs(node->data.var_defs);
+			case t_for_init_for_expr_list:
+				free_for_expr_list(node->data.expr_list);
+				break;
+		}
 
-	free(node);
-}
-
-void free_for_init_for_expr_list(is_for_init* node)
-{
-	free_for_expr_list(node->data.expr_list);
-
-	free(node);
+		free(node);
+	}
 }
 
 void free_for_inc(is_for_inc* node)
 {
-	/* TODO */
+	free_for_expr_list(node);
 }
 
 void free_func_call(is_func_call* node)
 {
-	/* TODO */
+	if (node)
+	{
+		switch (node->type)
+		{
+			case t_func_call_id:
+				free_id(node->id);
+				break;
+
+			case t_func_call_sysout:
+				break;
+		}
+
+		free_func_call_arg_list(node->args);
+		free(node);
+	}
 }
 
 void free_func_call_arg_list(is_func_call_arg_list* node)
 {
-	/* TODO */
-}
-
-void free_func_call_id(is_func_call* node)
-{
-	free_id(node->id);
-	free_func_call_arg_list(node->args);
-
-	free(node);
-}
-
-void free_func_call_sysout(is_func_call* node)
-{
-	free_func_call_arg_list(node->args);
-
-	free(node);
+	free_expr_list(node);
 }
 
 void free_func_def(is_func_def* node)
 {
-	free_type_decl(node->type);
-	free_id(node->id);
-	free_func_def_arg_list(node->args);
-	free_stmt_list(node->body);
+	if (node)
+	{
+		free_type_decl(node->type);
+		free_id(node->id);
+		free_func_def_arg_list(node->args);
+		free_stmt_list(node->body);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_func_def_arg(is_func_def_arg* node)
 {
-	free_type_decl(node->type);
-	free_id(node->id);
+	if (node)
+	{
+		free_type_decl(node->type);
+		free_id(node->id);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_func_def_arg_list(is_func_def_arg_list* node)
 {
-	free_func_def_arg(node->node);
-
-	if (node->next)
+	if (node)
+	{
+		free_func_def_arg(node->node);
 		free_func_def_arg_list(node->next);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_if(is_if* node)
 {
-	free_expr(node->cond);
-	free_stmt(node->then_body);
-	free_stmt(node->else_body);
+	if (node)
+	{
+		free_expr(node->cond);
+		free_stmt(node->then_body);
+		free_stmt(node->else_body);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_incr_op(is_incr_op* node)
 {
-	free_var(node->var);
+	if (node)
+	{
+		free_var(node->var);
 
-	free(node);
+		free(node);
+	}
 }
 
 void free_loop_stmt(is_loop_stmt* node)
 {
-	/* TODO */
-}
+	if (node)
+	{
+		switch (node->type)
+		{
+			case t_loop_stmt_for:
+				free_for(node->data.for_stmt);
+				break;
 
-void free_loop_stmt_for(is_loop_stmt* node)
-{
-	free_for(node->data.for_stmt);
+			case t_loop_stmt_while:
+				free_while(node->data.while_stmt);
+				break;
 
-	free(node);
-}
+			case t_loop_stmt_do_while:
+				free_do_while(node->data.do_while_stmt);
+				break;
+		}
 
-void free_loop_stmt_while(is_loop_stmt* node)
-{
-	free_while(node->data.while_stmt);
-
-	free(node);
-}
-
-void free_loop_stmt_do_while(is_loop_stmt* node)
-{
-	free_do_while(node->data.do_while_stmt);
-
-	free(node);
+		free(node);
+	}
 }
 
 void free_member_stmt(is_member_stmt* node)
 {
-	/* TODO */
-}
+	if (node)
+	{
+		switch (node->type)
+		{
+			case t_member_stmt_var:
+				free_var_stmt(node->data.var);
+				break;
 
-void free_member_stmt_var(is_member_stmt* node)
-{
-	free_var_stmt(node->data.var);
+			case t_member_stmt_func_def:
+				free_func_def(node->data.func_def);
+				break;
+		}
 
-	free(node);
-}
-
-void free_member_stmt_func_def(is_member_stmt* node)
-{
-	free_func_def(node->data.func_def);
-
-	free(node);
+		free(node);
+	}
 }
 
 void free_new_op(is_new_op* node)
