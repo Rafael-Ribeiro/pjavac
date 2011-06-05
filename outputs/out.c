c9382e9e220a25444bdb5601cf1fc14bf7596526
jprafael <jprafael@student.dei.uc.pt>
2011-06-05 16:44:59 +0100

    minor fixes; recursivity workink (clean)

--- outputs/out.c
+++ outputs/out.c
@@ -19,21 +19,68 @@ post_builtins: ;
 	goto label_1_postend;
 
 label_1:
-	; /* func2 */
+	; /* func */
 	_temp_frame = (FRAME*)malloc(sizeof(FRAME));
 	_temp_frame->parent = _fp;
 
+	_temp_frame->locals[0] = (int*)malloc(sizeof(int));
+	*(int*)(_temp_frame->locals[0]) = *(int*)(_fp->args[0]);
+
 	_fp = _temp_frame;
 
-	/* func2 body */
-	int _temp_0 = 1;
-	_temp_ret = &_temp_0;
-	goto label_1_end;
+	/* func body */
+	char* _temp_0 = strdup("");
+	int* _temp_1;
+	_temp_1 = (int*)_fp->locals[0];
+	int _temp_2 = *_temp_1;
 
+	/* conversion from int to string */
+	_fp->retaddr = 3;
+	_fp->args[0] = (int*)malloc(sizeof(int));
+	*(int*)_fp->args[0] = _temp_2;
+	goto int_to_string;
+
+label_3: ;
+	; /* temp_3 gets the string */
+	char* _temp_3 = (char*)_fp->retval;
+
+	/* string + string */
+	_fp->retaddr = 4;
+	_fp->args[0] = (char**)malloc(sizeof(char*));
+	_fp->args[1] = (char**)malloc(sizeof(char*));
+	*(char**)_fp->args[0] = _temp_0;
+	*(char**)_fp->args[1] = _temp_3;
+	goto string_concat;
+
+label_4:
+	; /* temp_4 gets the concatenated string */
+	char* _temp_4 = (char*)_fp->retval;
+
+
+	/* function call: System.out.println */
+	_fp->retaddr = 2;
+	_fp->args[0] = &_temp_4;
+	goto label_2147483647;
+
+label_2:
+	; /* end of func call*/
+	int* _temp_5;
+	_temp_5 = (int*)_fp->locals[0];
+	int _temp_6 = *_temp_5;
+
+	int _temp_7 = 1;
+	int _temp_8 = _temp_6 + _temp_7;
+
+	/* function call: func */
+	_fp->retaddr = 5;
+	_fp->args[0] = &_temp_8;
+	goto label_1;
+
+label_5:
+	; /* end of func call*/
 
 label_1_end:
-	/* end of func2 */
-	_fp->parent->retval = _temp_ret;
+	/* end of func */
 	_temp_frame = _fp->parent;
 	free(_fp);
 	_fp = _temp_frame;
@@ -41,39 +88,6 @@ label_1_end:
 	goto redirector;
 
 label_1_postend:
-	; /* post end of func2 */
-
-	goto label_2_postend;
-
-label_2:
-	; /* func */
-	_temp_frame = (FRAME*)malloc(sizeof(FRAME));
-	_temp_frame->parent = _fp;
-
-	_fp = _temp_frame;
-
-	/* func body */
-	/* function call: func2 */
-	_fp->retaddr = 3;
-	goto label_1;
-
-label_3:
-	; /* end of func call*/
-	int _temp_1 = *(int*)_fp->retval;
-	_temp_ret = &_temp_1;
-	goto label_2_end;
-
-
-label_2_end:
-	/* end of func */
-	_fp->parent->retval = _temp_ret;
-	_temp_frame = _fp->parent;
-	free(_fp);
-	_fp = _temp_frame;
-
-	goto redirector;
-
-label_2_postend:
 	; /* post end of func */
 
 	goto label_0_postend;
@@ -86,43 +100,14 @@ label_0:
 	_fp = _temp_frame;
 
 	/* main body */
-	char* _temp_2 = strdup("");
+	int _temp_9 = 0;
+
 	/* function call: func */
-	_fp->retaddr = 5;
-	goto label_2;
-
-label_5:
-	; /* end of func call*/
-	int _temp_3 = *(int*)_fp->retval;
-	/* conversion from int to string */
 	_fp->retaddr = 6;
-	_fp->args[0] = (int*)malloc(sizeof(int));
-	*(int*)_fp->args[0] = _temp_3;
-	goto int_to_string;
+	_fp->args[0] = &_temp_9;
+	goto label_1;
 
-label_6: ;
-	; /* temp_4 gets the string */
-	char* _temp_4 = (char*)_fp->retval;
-
-	/* string + string */
-	_fp->retaddr = 7;
-	_fp->args[0] = (char**)malloc(sizeof(char*));
-	_fp->args[1] = (char**)malloc(sizeof(char*));
-	*(char**)_fp->args[0] = _temp_2;
-	*(char**)_fp->args[1] = _temp_4;
-	goto string_concat;
-
-label_7:
-	; /* temp_5 gets the concatenated string */
-	char* _temp_5 = (char*)_fp->retval;
-
-
-	/* function call: System.out.println */
-	_fp->retaddr = 4;
-	_fp->args[0] = &_temp_5;
-	goto label_2147483647;
-
-label_4:
+label_6:
 	; /* end of func call*/
 
 label_0_end:
@@ -156,8 +141,6 @@ redirector:
 		goto label_5;
 	if (_fp->retaddr == 6)
 		goto label_6;
-	if (_fp->retaddr == 7)
-		goto label_7;
 
 label_end:
 	return 0;
